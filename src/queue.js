e66af3309c15c2dbdbae732925e0b12d5363c276
gogicha007 <gogicha@gmail.com>
2023-12-15 14:47:04 +0400

    chor: just save the work

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,19 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    return ++this.size;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) return null;
+    const temp = this.head;
+    if (this.head === this.tail) {
+      this.tail = null;
+    }
+    this.head = this.head.next;
+    this.size--;
+    return temp.value;
+  }
+
+  getUnderlyingList() {
+    return this.head;
   }
 }
 
--- test.js
+++ test.js
@@ -1,148 +1,41 @@
-// const { Node } = require('../extensions/list-tree.js');
-
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
   }
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.rootNode = null;
-  }
-
-  root() {
-    return this.rootNode;
-  }
-
-  add(data) {
-    const newNode = new Node(data);
-    if (!this.rootNode) {
-      this.rootNode = newNode;
-    } else {
-      this.insertNode(this.rootNode, newNode);
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
     }
-  }
 
-  insertNode(parent, newNode) {
-    if (newNode.data < parent.data) {
-      if (parent.left === null) {
-        parent.left = newNode;
-      } else {
-        this.insertNode(parent.left, newNode);
-      }
-    } else {
-      if (parent.right === null) {
-        parent.right = newNode;
-      } else {
-        this.insertNode(parent.right, newNode);
-      }
-    }
-  }
-
-  has(data) {
-    return !!this.search(this.root(), data);
-  }
-
-  find(data) {
-    return this.search(this.root(), data);
-  }
-
-  search(parent, data) {
-    if (!parent) {
-      return parent;
-    } else {
-      if (parent.data === data) {
-        return parent;
-      } else if (data < parent.data) {
-        return this.search(parent.left, data);
-      } else {
-        return this.search(parent.right, data);
-      }
-    }
-  }
-
-  max() {
-    return this.getMax(this.rootNode);
-  }
-
-  getMax(node) {
-    if (!node.right) {
-      return node.data;
-    } else {
-      return this.getMax(node.right);
-    }
-  }
-
-  min() {
-    return this.getMin(this.rootNode);
-  }
-
-  getMin(node) {
-    if (!node.left) {
-      return node.data;
-    } else {
-      return this.getMin(node.left);
-    }
-  }
-
-  remove(data) {
-    this.rootNode = this.removeNode(this.rootNode, data);
-  }
-
-  removeNode(root, data) {
-    if (root === null) {
-      return root;
-    }
-    if (data < root.data) {
-      root.left = this.removeNode(root.left, data);
-    } else if (data > root.data) {
-      root.right = this.removeNode(root.right, data);
-    } else {
-      if (!root.left && !root.right) {
-        return null;
-      }
-      if (!root.left) {
-        return root.right;
-      } else if (!root.right) {
-        return root.left;
-      }
-      root.data = this.getMin(root.right);
-      root.right = this.removeNode(root.right, root.data);
-    }
-    return root;
-  }
+    return new ListNode(cur);
+  }, null);
 }
 
-const tree = new BinarySearchTree();
-tree.add(9);
-tree.add(14);
-tree.add(2);
-tree.add(6);
-tree.add(128);
-tree.add(8);
-tree.add(31);
-tree.add(54);
-tree.add(1);
-// console.log(tree)
-tree.remove(14);
-tree.remove(9);
-tree.remove(8);
+function removeKFromList(l,k){
+  let currNode = l;
+  console.log(currNode.value)
+  let prevNode = null;
+  if(currNode.value === k){
+    prevNode = currNode.next;
+  } else {
+      while(currNode.value !== k) {
+        prevNode = currNode;
+        currNode = currNode.next;
+      }
+      prevNode.next = currNode.next;
+  }
+  return prevNode
+}
 
-console.log(tree.has(14));
-// console.log(tree)
-console.log(tree.has(8));
-console.log(tree.has(9));
-// console.log(tree.has(2));
-// console.log(tree.has(6));
-// console.log(tree.has(128));
-// console.log(tree.has(31));
-// console.log(tree.has(54));
-// console.log(tree.has(1))
-// console.log(tree)
 
-// module.exports = {
-//   BinarySearchTree,
-// };
+
+const initial = convertArrayToList([1, 2, 3, 3, 4, 5]);
+console.log(removeKFromList(initial, 3))
+console.log(initial)
+
