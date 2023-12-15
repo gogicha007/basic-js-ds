4da6e783ba33df8399a917590187d9c9f4917a23
gogicha007 <gogicha@gmail.com>
2023-12-15 21:53:34 +0400

    feat: remove-from-list task done

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let currNode = l;
+  let loop = 0;
+  let prevNode;
+  while (currNode) {
+    if (currNode.value === k) {
+      if (loop === 0) {
+        l = currNode.next;
+      } else {
+        prevNode.next = currNode.next;
+      }
+    } else {
+      prevNode = currNode;
+    }
+    currNode = currNode.next;
+    loop++;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
--- test.js
+++ test.js
@@ -12,31 +12,56 @@ function convertArrayToList(arr) {
       node.next = acc;
       return node;
     }
-
     return new ListNode(cur);
   }, null);
 }
 
-function removeKFromList(l,k){
+function getSize(l) {
+  let size = 0;
   let currNode = l;
-  let prevNode = null;
-  while(currNode.next !== null){
-    if(currNode.value === k){
-      prevNode = currNode.next;
-      break
-    } else {
-        while(currNode.next !== null) {
-          prevNode = currNode;
-          currNode = currNode.next;
-        }
-        prevNode.next = currNode.next;
-    }
+  while (currNode) {
+    currNode = currNode.next;
+    size++;
   }
-  return l
+  return size;
+}
+
+function countValue(l, k) {
+  let currNode = l;
+  let indexes = [];
+  let loop = 0;
+  while (currNode) {
+    if (currNode.value === k) {
+      indexes.push(loop);
+    }
+    currNode = currNode.next;
+    loop++;
+  }
+  return indexes;
 }
 
 
-const initial = convertArrayToList([1,2, 3, 3, 4, 5]);
-console.log(removeKFromList(initial, 3))
-console.log(initial)
+function removeValue(l,k){
+  let currNode = l;
+  let loop = 0;
+  let prevNode;
+  while(currNode) {
+    if(currNode.value === k) {
+      if(loop === 0) {
+        l = currNode.next
+      } else {
+        prevNode.next = currNode.next
+      }
+    } else {
+      prevNode = currNode;
+    }
+    currNode = currNode.next
+    loop++
+  }
+  return l;
+}
 
+const initial = convertArrayToList([3, 2, 3, 3, 4, 5]);
+// console.log(countValue(initial, 3));
+// console.log(getSize(initial));
+console.log(removeValue(initial, 3))
